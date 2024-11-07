b522dafb2a72f3aaa9bfe5016b0434f42a4b24f3
andynaistat <naistat.a@gmail.com>
2024-11-06 22:32:17 -0300

    fix bark

--- Code/Server/Bark.py
+++ Code/Server/Bark.py
@@ -1,20 +1,16 @@
-import pygame
+from pydub import AudioSegment
+from pydub.playback import play
 
-class Bark:
-    def __init__(self):
-        # Inicializar el módulo de mezcla de pygame
-        pygame.mixer.init()
+class AudioPlayer:
+    def __init__(self, audio_file):
+        self.audio_file = audio_file
 
-    def run(self, archivo):
-        """Carga y reproduce un archivo de audio."""
-        try:
-            pygame.mixer.music.load(archivo)
-            pygame.mixer.music.play()
+    def play(self):
+        sound = AudioSegment.from_file(self.audio_file)
+        play(sound)
 
-            # Esperar a que el audio termine de reproducirse
-            while pygame.mixer.music.get_busy():
-                continue
-
-        except pygame.error as e:
-            print(f"Error al reproducir el archivo de audio: {e}")
+# Uso
+if __name__ == "__main__":
+    player = AudioPlayer('../Audios/small-dog-barking.mp3')
+    player.play()
 
